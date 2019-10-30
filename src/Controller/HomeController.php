89aed14471144382088f79e718696e0606ba4e50
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2019-10-30 13:25:43 +0100

    Create simple home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
